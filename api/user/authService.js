816456f02997c42d74823e5a2f9f9ee0caa161d8
fernando Pimentel <ferpiolli@gmail.com>
2018-06-16 21:35:45 -0300

    validação com token

--- api/user/authService.js
+++ api/user/authService.js
@@ -29,6 +29,7 @@ User.findOne({email}, (err, user) => {
     } else if (user && bcrypt.compareSync(password, user.password)) {
         const token = jwt.sign(user, env.authSecret, {
             expiresIn: "1 day"
+            //expiresIn: "10 seconds"
         })
         const { name, email } = user
         res.json({ name, email, token })
